12eb3efb0381a34077f074cfc8308edab5f54183
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2022-09-14 09:08:49 -0300

    Initial project

---
+++ GoodWeather/GoodWeather/ViewController/AddWeatherCityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by Paulo Henrique Bendazzoli on 14/09/22.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeather/GoodWeather/ViewController/WeatherListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Paulo Henrique Bendazzoli on 14/09/22.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+}
